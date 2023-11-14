ee4e2bea0de814624312a1a391cf924f5b636ed9
Venn Dominic A. Dela Peña <134032009+VennDominicAdelaPena@users.noreply.github.com>
2023-11-14 21:23:20 +0800

    Add files via upload

--- Image Processing/Form1.Designer.cs
+++ Image Processing/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
             menuStrip1 = new MenuStrip();
@@ -41,28 +42,46 @@
             button4 = new Button();
             saveFileDialog1 = new SaveFileDialog();
             button5 = new Button();
+            pictureBox3 = new PictureBox();
+            button6 = new Button();
+            button7 = new Button();
+            openFileDialog2 = new OpenFileDialog();
+            openFileDialog3 = new OpenFileDialog();
+            openFileDialog4 = new OpenFileDialog();
+            openFileDialog5 = new OpenFileDialog();
+            button8 = new Button();
+            button9 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            comboBox1 = new ComboBox();
+            button10 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(43, 39);
+            pictureBox1.Location = new Point(43, 38);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(532, 483);
+            pictureBox1.Size = new Size(531, 483);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(646, 39);
+            pictureBox2.Location = new Point(1229, 38);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(532, 483);
+            pictureBox2.Size = new Size(531, 483);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // menuStrip1
             // 
@@ -70,7 +89,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1241, 33);
+            menuStrip1.Size = new Size(1818, 33);
             menuStrip1.TabIndex = 2;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -102,9 +121,9 @@
             // 
             // button1
             // 
-            button1.Location = new Point(646, 541);
+            button1.Location = new Point(43, 628);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(111, 33);
             button1.TabIndex = 3;
             button1.Text = "Copy";
             button1.UseVisualStyleBackColor = true;
@@ -112,9 +131,9 @@
             // 
             // button2
             // 
-            button2.Location = new Point(764, 541);
+            button2.Location = new Point(43, 667);
             button2.Name = "button2";
-            button2.Size = new Size(112, 34);
+            button2.Size = new Size(111, 33);
             button2.TabIndex = 4;
             button2.Text = "GreyScale";
             button2.UseVisualStyleBackColor = true;
@@ -122,9 +141,9 @@
             // 
             // button3
             // 
-            button3.Location = new Point(882, 541);
+            button3.Location = new Point(43, 706);
             button3.Name = "button3";
-            button3.Size = new Size(112, 34);
+            button3.Size = new Size(111, 33);
             button3.TabIndex = 5;
             button3.Text = "Invert";
             button3.UseVisualStyleBackColor = true;
@@ -132,9 +151,9 @@
             // 
             // button4
             // 
-            button4.Location = new Point(1000, 541);
+            button4.Location = new Point(190, 628);
             button4.Name = "button4";
-            button4.Size = new Size(112, 34);
+            button4.Size = new Size(111, 33);
             button4.TabIndex = 6;
             button4.Text = "Histogram";
             button4.UseVisualStyleBackColor = true;
@@ -142,19 +161,148 @@
             // 
             // button5
             // 
-            button5.Location = new Point(1117, 541);
+            button5.Location = new Point(190, 667);
             button5.Name = "button5";
-            button5.Size = new Size(112, 34);
+            button5.Size = new Size(111, 33);
             button5.TabIndex = 7;
             button5.Text = "Sepia";
             button5.UseVisualStyleBackColor = true;
             button5.Click += button5_Click;
             // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(643, 38);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(531, 483);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(206, 538);
+            button6.Name = "button6";
+            button6.Size = new Size(116, 33);
+            button6.TabIndex = 9;
+            button6.Text = "Load Image";
+            button6.TextAlign = ContentAlignment.BottomLeft;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(811, 538);
+            button7.Name = "button7";
+            button7.Size = new Size(160, 33);
+            button7.TabIndex = 10;
+            button7.Text = "Load Background";
+            button7.TextAlign = ContentAlignment.BottomLeft;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // openFileDialog2
+            // 
+            openFileDialog2.FileName = "openFileDialog1";
+            // 
+            // openFileDialog3
+            // 
+            openFileDialog3.FileName = "openFileDialog1";
+            // 
+            // openFileDialog4
+            // 
+            openFileDialog4.FileName = "openFileDialog1";
+            // 
+            // openFileDialog5
+            // 
+            openFileDialog5.FileName = "openFileDialog5";
+            openFileDialog5.FileOk += openFileDialog5_FileOk;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(190, 706);
+            button8.Name = "button8";
+            button8.Size = new Size(111, 33);
+            button8.TabIndex = 11;
+            button8.Text = "Subtraction";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(377, 667);
+            button9.Name = "button9";
+            button9.Size = new Size(161, 33);
+            button9.TabIndex = 12;
+            button9.Text = "Turn on Camera";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Copy", "GreyScale", "Invert", "Sepia", "Subtraction" });
+            comboBox1.Location = new Point(377, 629);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(183, 33);
+            comboBox1.TabIndex = 13;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(377, 706);
+            button10.Name = "button10";
+            button10.Size = new Size(161, 33);
+            button10.TabIndex = 14;
+            button10.Text = "Turn Off Camera";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(86, 585);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 25);
+            label1.TabIndex = 15;
+            label1.Text = "Buttons for DPI 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(389, 585);
+            label2.Name = "label2";
+            label2.Size = new Size(149, 25);
+            label2.TabIndex = 16;
+            label2.Text = "Buttons for DPI 2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(584, 632);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 25);
+            label3.TabIndex = 17;
+            label3.Text = "Choose Process";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1241, 675);
+            ClientSize = new Size(1818, 764);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button10);
+            Controls.Add(comboBox1);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(pictureBox3);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
@@ -166,10 +314,12 @@
             MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -189,5 +339,20 @@
         private Button button4;
         private SaveFileDialog saveFileDialog1;
         private Button button5;
+        private PictureBox pictureBox3;
+        private Button button6;
+        private Button button7;
+        private OpenFileDialog openFileDialog2;
+        private OpenFileDialog openFileDialog3;
+        private OpenFileDialog openFileDialog4;
+        private OpenFileDialog openFileDialog5;
+        private Button button8;
+        private Button button9;
+        private System.Windows.Forms.Timer timer1;
+        private ComboBox comboBox1;
+        private Button button10;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Image Processing/Form1.cs
+++ Image Processing/Form1.cs
@@ -1,11 +1,19 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WebCamLib;
 namespace Image_Processing
 {
     public partial class Form1 : Form
     {
         Bitmap loaded, processed;
+        Bitmap imageA, colorgreen; //imageB=loaded
+        private Device webCam;
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Interval = 1000;
         }
 
         private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
@@ -29,6 +37,9 @@ namespace Image_Processing
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            loaded = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(loaded, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
             processed = new Bitmap(loaded.Width, loaded.Height);
             Color pixel;
             for (int i = 0; i < loaded.Width; i++)
@@ -140,6 +151,212 @@ namespace Image_Processing
                 }
             }
             pictureBox2.Image = processed;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            openFileDialog5.ShowDialog();
+        }
+
+        private void openFileDialog5_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            imageA = new Bitmap(openFileDialog5.FileName);
+            pictureBox3.Image = imageA;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //imageA = new Bitmap(openFileDialog5.FileName);
+            //pictureBox3.Image = imageA;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color mygreen = Color.FromArgb(0, 0, 255);
+            int greygreen = (mygreen.R + mygreen.G + mygreen.B) / 3;
+            int threshold = 5;
+
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    Color backpixel = imageA.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    int subtractvalue = Math.Abs(grey - greygreen);
+                    if (subtractvalue > threshold)
+                    {
+                        processed.SetPixel(x, y, pixel);
+                    }
+                    else
+                    {
+                        processed.SetPixel(x, y, backpixel);
+                    }
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            StartWebcam();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartWebcam()
+        {
+            webCam = new Device();
+            webCam.ShowWindow(pictureBox1);
+            timer1.Start();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopWebcam();
+        }
+
+        private void StopWebcam()
+        {
+            webCam?.Stop();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Color pixel;
+            if (webCam != null)
+            {
+                // Capture the current frame from the webcam
+                IDataObject data = Clipboard.GetDataObject();
+                if (data != null && data.GetDataPresent(DataFormats.Bitmap))
+                {
+                    loaded = (Bitmap)data.GetData(DataFormats.Bitmap);
+
+                    processed = new Bitmap(loaded.Width, loaded.Height);
+
+                }
+                switch (comboBox1.SelectedItem?.ToString())
+                {
+                    case "Copy":
+                        webCam.Sendmessage();
+                        for (int i = 0; i < loaded.Width; i++)
+                        {
+                            for (int j = 0; j < loaded.Height; j++)
+                            {
+                                pixel = loaded.GetPixel(i, j);
+                                processed.SetPixel(i, j, pixel);
+                            }
+                        }
+                        pictureBox2.Image = processed;
+
+                        break;
+                    case "GreyScale":
+                        webCam.Sendmessage();
+                        int grey;
+                        for (int i = 0; i < loaded.Width; i++)
+                        {
+                            for (int j = 0; j < loaded.Height; j++)
+                            {
+                                pixel = loaded.GetPixel(i, j);
+                                grey = (pixel.R + pixel.G + pixel.B) / 3;
+                                processed.SetPixel(i, j, Color.FromArgb(grey, grey, grey));
+                            }
+                        }
+                        pictureBox2.Image = processed;
+                        break;
+                    case "Invert":
+                        webCam.Sendmessage();
+                        for (int i = 0; i < loaded.Width; i++)
+                        {
+                            for (int j = 0; j < loaded.Height; j++)
+                            {
+                                pixel = loaded.GetPixel(i, j);
+                                processed.SetPixel(i, j, Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B));
+                            }
+                        }
+                        pictureBox2.Image = processed;
+                        break;
+                    case "Sepia":
+                        webCam.Sendmessage();
+                        int r, g, b, new_r, new_g, new_b;
+                        for (int i = 0; i < loaded.Width; i++)
+                        {
+                            for (int j = 0; j < loaded.Height; j++)
+                            {
+                                pixel = loaded.GetPixel(i, j);
+                                r = pixel.R;
+                                b = pixel.B;
+                                g = pixel.G;
+                                new_r = Math.Min(((int)(.393 * r + .768 * g + .189 * b)), 255);
+                                new_g = Math.Min(((int)(.349 * r + .686 * g + .168 * b)), 255);
+                                new_b = Math.Min(((int)(.272 * r + .534 * g + .131 * b)), 255);
+                                processed.SetPixel(i, j, Color.FromArgb(new_r, new_g, new_b));
+                            }
+                        }
+                        pictureBox2.Image = processed;
+                        break;
+                    case "Subtraction":
+                        webCam.Sendmessage();
+                        Color mygreen = Color.FromArgb(0, 0, 255);
+                        int greygreen = (mygreen.R + mygreen.G + mygreen.B) / 3;
+                        int threshold = 5;
+
+                        for (int x = 0; x < loaded.Width; x++)
+                        {
+                            for (int y = 0; y < loaded.Height; y++)
+                            {
+                                pixel = loaded.GetPixel(x, y);
+                                Color backpixel = imageA.GetPixel(x, y);
+                                grey = (pixel.R + pixel.G + pixel.B) / 3;
+                                int subtractvalue = Math.Abs(grey - greygreen);
+                                if (subtractvalue > threshold)
+                                {
+                                    processed.SetPixel(x, y, pixel);
+                                }
+                                else
+                                {
+                                    processed.SetPixel(x, y, backpixel);
+                                }
+                            }
+                        }
+                        pictureBox2.Image = processed;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            webCam.Stop();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
         }
     }
 }
